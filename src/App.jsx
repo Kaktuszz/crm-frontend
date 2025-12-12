585ed86c8faef3c861c27cc953836f76026c0c73
Bohdan Fedirko <bohdan.fedirko@student.uj.edu.pl>
2025-12-12 13:49:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { Navbar } from './components/Navbar/Navbar';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
